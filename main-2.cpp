7af53609a884df772be87b9f42d83d4f0935ec3b
jasmin <jajalorino@gmail.com>
2025-07-17 11:56:24 +0800

    added docu of keywords

--- main-2.cpp
+++ main-2.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#define LIMIT 5
 using namespace std;
+
+char queue[LIMIT];
+
+/*  isEmpty() - checks if the queue is empty
+    isFull() - checks if the queue is full
+    enqueue() - adds an element to the end of the queue
+    dequeue() - removes an element from the front of the queue
+    peek() - returns the front element of the queue without removing it
+    size() - returns the number of elements in the queue
+    display() - displays all elements in the queue
+*/
 
 int main()
 {
