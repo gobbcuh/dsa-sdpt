8b46516f6521204a685ef75eee3d29c93137d015
jasmin <jajalorino@gmail.com>
2025-07-17 09:03:22 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#define LIMIT 5
+using namespace std;
+
+char stack[LIMIT];
+
+/*  isEmpty() - checks if the stack is empty
+    isFull() - checks if the stack is full
+    push(x) - adds a value on the top of the stack
+    pop() - removes the top value from the stack
+    peek() - returns the top value of the stack
+    size() - returns the number of elements in the stack
+    display() - displays the elements of the stack  */
+
+int main()
+{
+}
